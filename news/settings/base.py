24d803c16a0fcadac708f485668d4cf0689a2545
Suraj Thapa <surajthapafc@gmail.om>
2020-08-27 13:41:27 +0530

    (Added) REdis Caching

--- news/settings/base.py
+++ news/settings/base.py
@@ -114,3 +114,15 @@ FILE_UPLOAD_PERMISSIONS = 0o644
 
 # To use Pillow (image optimizer)
 OPTIMIZED_IMAGE_METHOD = "pillow"
+
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient"
+        },
+
+    }
+}
