44ea1f76ea446c8a1cd97e348f70d03953e96c0d
Suraj Thapa <surajthapafc@gmail.com>
2021-03-23 14:37:57 +0530

    (UPdated) final version

--- news/settings/local.py
+++ news/settings/local.py
@@ -17,12 +17,18 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": "test",
-        "USER": "testuser",
+        "USER": "fc",
         "PASSWORD": "Admin@123",
         "HOST": "localhost",
         "PORT": "5432",
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Static files (CSS, JavaScript, Images)
